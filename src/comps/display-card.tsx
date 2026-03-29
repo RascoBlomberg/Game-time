a7e3f73e6c0717936c2c19f47fca05ed455db305
Oscar <Oscar.Blomberg1@medieinstitutet.se>
2026-03-29 22:15:30 +0200

    added styleing to display cards

--- src/comps/display-card.tsx
+++ src/comps/display-card.tsx
@@ -9,12 +9,12 @@ type Props = {
 
 export default function DisplayCard({ title, link, image, description }: Props) {
   return (
-    <div>
-      <img src={image} alt={title}/>
-      <div>
-        <h2>{title}</h2>
-        {description && (<p>{description}</p>)}
-        <Link to={link}> Open</Link>
+    <div className="bg-gray-800 text-white rounded-xl overflow-hidden shadow-lg hover:bg-gray-600 transition duration-300 w-72">
+      <img src={image} alt={title} className="w-full h-auto object-cover"/>
+      <div className="p-4 flex flex-col gap-2">
+        <h2 className="text-lg font-bold">{title}</h2>
+        {description && (<p className="text-sm text-gray-400">{description}</p>)}
+        <Link to={link} className="mt-2 inline-block bg-blue-600 hover:bg-blue-700 text-white text-center py-2 rounded-lg transition"> Open</Link>
       </div>
     </div>
   );
--- src/pages/gamePage.tsx
+++ src/pages/gamePage.tsx
@@ -1,10 +1,21 @@
 import DisplayCard from "../comps/display-card";
 import Navbar from "../comps/navbar";
-export default function GamePage () {
-    return (
-        <div>
-            <Navbar />
-            <DisplayCard title="Clash of Clans" link="/coc" image="src\assets\hero.png"/>
-        </div>
-    )
+export default function GamePage() {
+  return (
+    <div>
+      <Navbar />
+      <div className="flex gap-6 p-2 m-2">
+        <DisplayCard
+          title="Clash of Clans"
+          link="/coc"
+          image="src\assets\hero.png"
+        />
+        <DisplayCard
+          title="Clash of Clans"
+          link="/coc"
+          image="src\assets\hero.png"
+        />
+      </div>
+    </div>
+  );
 }
